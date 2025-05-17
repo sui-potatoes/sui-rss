aeb1802ddb6038e81b27fcc185d73f902038b0d2
Damir Shamanaev <damirka.ru@gmail.com>
2025-05-17 16:00:25 +0400

    vercel?

--- app/src/api/hello.ts
+++ app/src/api/hello.ts
@@ -1,4 +1,4 @@
-export default function handler(req: Request): Response | Promise<Response> {
+export default function handler(_req: Request): Response | Promise<Response> {
     return new Response(JSON.stringify({ message: "Hello from API" }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
